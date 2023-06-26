bd961d878b3d99b0bc0806d764381454600718f9
Dmitry <09.09.2005@inbox.ru>
2023-06-26 06:23:28 +0400

    Reset

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let count = 0;
-
 function changeCount(num) {
   count += num;
+  document.getElementById("count").innerHTML = count;
+}
+function Reset() {
+  count = 0;
   document.getElementById("count").innerHTML = count;
 }
